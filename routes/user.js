6d317023c771a430412acb99fc115a68bdb45b66
badai89 <zerobadai5@gmail.com>
2019-09-25 16:13:54 +0700

    mongoose atlas

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 const { addUser, getUser } = require('../controller/user');
+
+app.get("/", (req, res, next) => res.send("Still practice!"));
 
 router.post("/userpost", addUser);
 router.get("/user", getUser);
